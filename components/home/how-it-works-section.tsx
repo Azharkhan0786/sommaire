e96c42c86b8854f20c8a99dc60ddd3de3f78f094
Azhar Khan <azharkhan844716@gmail.com>
2025-11-04 02:06:11 +0530

    commit

--- components/home/how-it-works-section.tsx
+++ components/home/how-it-works-section.tsx
@@ -1,5 +1,17 @@
 export default function HowItWorksSection() {
     return(
-        <h1>How It Works</h1>
+        <section className="relative overflow-hidden bg-gray-50">
+             <div className="py-12 lg:py-20 mx-auto px-4   sm:px-6 lg:px-8 max-w-5xl lg:pt-12">
+                </div>
+
+        <div className="text-center mb-16">
+            <h2 className="font-bold text-xl uppercase mb-4 text-rose-500">How it works</h2>
+            <h3 className="font-bold text-3xl max-w-2xl mx-auto">Transform any PDF into an easy-to-digital summary in three simple steps</h3>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl mx-auto relative">
+
+        </div>
+        </section>
     )
 }
