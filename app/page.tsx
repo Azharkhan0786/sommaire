762972eff122d4807b214fad5a745a0e686b636c
Azhar Khan <azharkhan844716@gmail.com>
2025-11-13 23:38:51 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,19 @@ import HeroSection from "@/components/home/hero-section";
 import BgGradient from "@/components/common/bg-gradient";
 import DemoSection from "@/components/home/demo-section";
 import HowItWorksSection from "@/components/home/how-it-works-section";
+import PricingSection from "@/components/home/pricing-section";
 
 export default function Home() {
   return (
     <div className="relative w-full">
-      <BgGradient />
+      {/* <BgGradient /> */}
       <div className="flex flex-col">
         <HeroSection />
         <DemoSection />
       </div>
       <HowItWorksSection />
-      {/* <PricingSection />
-      <CTASection/> */}
+       <PricingSection />
+     
     </div>
   );
 }
