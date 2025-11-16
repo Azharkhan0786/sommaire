4f48f93cff45a85ca895d671a2c7ba62413eaf87
Azhar Khan <azharkhan844716@gmail.com>
2025-11-16 14:17:51 +0530

    commit

--- components/home/pricing-section.tsx
+++ components/home/pricing-section.tsx
@@ -1,3 +1,16 @@
 export default function PricingSection() {
-    return <section>PricingSection</section>
+    return <section>
+        <div>
+             <div className="py-12 lg:py-20 mx-auto px-4   sm:px-6 lg:px-8 max-w-5xl lg:pt-12">
+                <div>
+                    <h2>Pricing</h2>
+                </div>
+                <div className="relative flex justify-center flex-col lg:flex-row items-center lg:items-stretch gap-8">
+                    {[].map((plan) => (
+                        <PricingCard key={plan.id} {...plan}/>
+                    ))}
+                </div>
+        </div>
+        </div>
+    </section>
 }
