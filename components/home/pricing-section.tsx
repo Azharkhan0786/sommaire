10024c6ec566db24399b37a14cd36279b4d884dc
Azhar Khan <azharkhan844716@gmail.com>
2025-11-16 18:08:53 +0530

    commit

--- components/home/pricing-section.tsx
+++ components/home/pricing-section.tsx
@@ -1,16 +1,90 @@
-export default function PricingSection() {
-    return <section>
+import Link from "next/link";
+
+type PriceType = {
+  name: string;
+  price: number | string;
+  description: string;
+  items: string[];
+  id: string;
+  paymentLink: string;
+  priceId: string;
+};
+
+const plans = [
+  {
+    name: "basic",
+    price: "$9/month",
+    description: "Perfect for occasional use",
+    items: [
+      "5PDF summaries per month",
+      "Standard processing speed",
+      "Email support",
+    ],
+    id: "basic",
+    paymentLink: "",
+    priceId: "",
+  },
+  {
+    name: "pro",
+    price: "$19/month",
+    description: "Perfect for professionals and teams",
+    items: [
+      "Unlimited PDF summaries",
+      "Priority processing speed",
+      "24/7 priority support",
+      "Markdown Export",
+    ],
+    id: "pro",
+    paymentLink: "",
+    priceId: "",
+  },
+];
+
+const PricingCard = ({
+  name,
+  price,
+  description,
+  items,
+  id,
+  paymentLink,
+}: PriceType )=> {
+  return (
+    <div className="relative w-full max-w-lg">
+      <div>
+        <p className="text-lg lg:text-xl font-bold capitalize">{name}</p>
+        <p className="text-base-content/80 mt-2">{description}</p>
+      </div>
+
+      <div>
+        <p>{price}</p>
         <div>
-             <div className="py-12 lg:py-20 mx-auto px-4   sm:px-6 lg:px-8 max-w-5xl lg:pt-12">
-                <div>
-                    <h2>Pricing</h2>
-                </div>
-                <div className="relative flex justify-center flex-col lg:flex-row items-center lg:items-stretch gap-8">
-                    {[].map((plan) => (
-                        <PricingCard key={plan.id} {...plan}/>
-                    ))}
-                </div>
+          <div>
+            {items.map((item, idx) => (
+              <li key={idx}>{item}</li>
+            ))}
+          </div>
+          <Link href={paymentLink}>Buy Now</Link>
         </div>
+      </div>
+    </div>
+  );
+};
+
+export default function PricingSection() {
+  return (
+    <section>
+      <div>
+        <div className="py-12 lg:py-24 mx-auto px-4   sm:px-6 lg:px-8 max-w-5xl lg:pt-12">
+          <div>
+            <h2>Pricing</h2>
+          </div>
+          <div className="relative flex justify-center flex-col lg:flex-row items-center lg:items-stretch gap-8">
+            {plans.map((plan)=> (
+              <PricingCard key={plan.id} {...plan} />
+            ))}
+          </div>
         </div>
+      </div>
     </section>
+  );
 }
