3d204212fb081f9a89ba69d949658df1be6a378e
Azhar Khan <azharkhan844716@gmail.com>
2025-11-16 19:08:07 +0530

    commit

--- components/home/pricing-section.tsx
+++ components/home/pricing-section.tsx
@@ -14,7 +14,7 @@ type PriceType = {
 const plans = [
   {
     name: "basic",
-    price: "$9/month",
+    price: "9",
     description: "Perfect for occasional use",
     items: [
       "5PDF summaries per month",
@@ -27,7 +27,7 @@ const plans = [
   },
   {
     name: "pro",
-    price: "$19/month",
+    price: "19",
     description: "Perfect for professionals and teams",
     items: [
       "Unlimited PDF summaries",
@@ -51,12 +51,11 @@ const PricingCard = ({
 }: PriceType) => {
   return (
     <div className="relative w-full max-w-lg">
-      <div className={cn("relative flex flex-col h-full z-10 gap-4 lg:gap-8 p-8 rounded-2xl border-[1px] border-gray-500/20",
-      id==='pro' && 'border-rose-500 gap-5 border-2'
+      <div className={cn(" relative flex flex-col  gap-4 lg:gap-8 p-8  h-full  border-[1px] border-gray-500/20 rounded-2xl",
+        id === "pro" && "border-rose-500 border-2 gap-5"
       )}>
         <div className="flex justify-between items-center gap-4">
           <div>
-            {" "}
             <p className="text-lg lg:text-xl font-bold capitalize">{name}</p>
             <p className="text-base-content/80 mt-2">{description}</p>
           </div>
@@ -64,17 +63,19 @@ const PricingCard = ({
 
         <div>
           <p>{price}</p>
-          <div>
+          </div>
             <div>
               {items.map((item, idx) => (
                 <li key={idx}>{item}</li>
               ))}
             </div>
-            <Link href={paymentLink}>Buy Now</Link>
+            <div>
+              <Link href={paymentLink}>Buy Now</Link>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+     
+    
   );
 };
 
