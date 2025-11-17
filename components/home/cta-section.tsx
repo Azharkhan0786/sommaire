385e33e7a030af5b8d7dda87355048cb7ed9f78c
Azhar Khan <azharkhan844716@gmail.com>
2025-11-18 00:36:00 +0530

    commit

--- components/home/cta-section.tsx
+++ components/home/cta-section.tsx
@@ -1,23 +1,36 @@
-import Link from 'next/link';
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { ArrowRight } from 'lucide-react';
+import { ArrowRight } from "lucide-react";
 
 export default function CTASection() {
   return (
     <section className=" bg-gray-50 py-12">
       <div className="py-12 lg:py-20 mx-auto px-4   sm:px-6 lg:px-8 max-w-5xl lg:pt-12">
         <div className=" flex flex-col items-center justify-center space-y-4 text-center">
-          <div>
-            <h2>Ready to Save Hours of Reading Time ?</h2>
-            <p>
+          <div className="space-y-2">
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+              Ready to Save Hours of Reading Time ?
+            </h2>
+            <p className="mx-auto max-w-2xl text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
               Transform Lengthy documents into clear, actionable insights with
               our AI-powered summarizer
             </p>
           </div>
           <div>
             <div>
-              <Button variant={'link'} className='bg-rose-500 w-full min-[400px]:w-auto bg-linear-to-r from-slate-900 to-rose-500 hover:from-rose-500 hover:to-slate-900 hover:text-white text-white transition-all duration-300 '>
-                <Link href="/#pricing"> Get Started {' '} <ArrowRight  className='ml-2 h-4 w-4 animate-pulse'/></Link>
+              <Button
+              size={'lg'}
+                variant={"link"}
+                className="bg-rose-500 w-full min-[400px]:w-auto bg-linear-to-r from-slate-900 to-rose-500 hover:from-rose-500 hover:to-slate-900 hover:text-white text-white transition-all duration-300 "
+              >
+                <Link
+                  href="/#pricing"
+                  className="flex items-center justify-center px-6"
+                >
+                  {" "}
+                  Get Started{" "}
+                  <ArrowRight className="ml-2 h-4 w-4 animate-pulse" />
+                </Link>
               </Button>
             </div>
           </div>
