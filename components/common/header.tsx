83e79ce1405ef1c0a5432df8ee4eecd80ead9465
Azhar Khan <azharkhan844716@gmail.com>
2025-11-18 12:52:57 +0530

    commit

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,15 +1,17 @@
 import { FileText } from "lucide-react";
 import NavLink from "./nav-link";
-import {Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Header() {
-  const isLoggedIn = false;
-
   return (
     <nav className="container flex items-center justify-between py-4 px-2 mx-auto lg:px-8">
       <div className="flex">
-        <NavLink href="/" className="flex items-center gap-1  shrink-0 lg:gap:2">
+        <NavLink
+          href="/"
+          className="flex items-center gap-1  shrink-0 lg:gap:2"
+        >
           <FileText className="h-5 w-5 lg:w-8 lg:h-8 text-gray-900 hover:rotate-12 transition-transform duration-200 ease-in-out" />
           <span className="font-extrabold lg:text-xl text-green-900">
             Sommaire
@@ -19,21 +21,26 @@ export default function Header() {
 
       <div className="flex gap-4 lg:justify-center lg:items-center">
         <NavLink href="/pricing">Pricing</NavLink>
-        {isLoggedIn && <NavLink href="/dashboard">Your Summaries</NavLink>}
+
+        <SignedIn>
+          <NavLink href="/dashboard">Your Summaries</NavLink>
+        </SignedIn>
       </div>
 
       <div className="flex lg:justify-end">
-        {isLoggedIn ? (
+        <SignedIn>
           <div className="flex gap-2 items-center">
             <NavLink href="/upload">Upload a PDF</NavLink>
             <div>Pro</div>
-            <div>User</div>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
           </div>
-        ) : (
-          <div>
-            <NavLink href="/sign-in">Sign In</NavLink>
-          </div>
-        )}
+        </SignedIn>
+
+        <SignedOut>
+          <NavLink href="/sign-in">Sign In</NavLink>
+       </SignedOut>
       </div>
     </nav>
   );
