f64df81a9855c99a43b5f6fa8cf998b16f61b14c
Azhar Khan <azharkhan844716@gmail.com>
2025-11-19 11:55:52 +0530

    commit

--- app/(logged-in)/upload/page.tsx
+++ app/(logged-in)/upload/page.tsx
@@ -1,33 +1,15 @@
-import { Badge } from "@/components/ui/badge";
-import { Sparkles } from "lucide-react";
+import UploadHeader from "@/components/upload/upload-header";
+import UploadForm from "@/components/upload/upload-form";
+
 
 export default function Page() {
   return (
     <section className="min-h-screen">
       <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
-        <div className="flex flex-col items-center justify-center gap-6 text-center">
-          <div className="relative  p-[1px]rounded-full bg-linear-to-r overflow-hidden from-rose-200 via-red-500 to-rose-800 animate-gradient-x group">
-            <Badge
-              variant={"secondary"}
-              className="relative px-6 py-2 text-rose-500 font-medium bg-white rounded-full group-hover:bg-gray-50 transition-colors   "
-            >
-              <Sparkles />
-              <span>AI-powered Content Creation</span>
-            </Badge>
-          </div>
-          <div className="capitalize text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Start Uploading{" "}
-            <span className="relative inline-block">
-              {" "}
-              <span className="relative z-10 px-2">Your PDF's</span>
-              <span
-                className="absolute inset-0 bg-rose-200/50 -rotate-2 rounded-lg transform -skew-y-1"
-                aria-hidden="true"
-              ></span>
-            </span>{" "}
-          </div>
-          <div><p>Upload your PDF and let our AI do the magic ✨</p></div>
-        </div>
+        <div className="flex flex-col justify-center items-center  text-center gap-6">
+       <UploadHeader />
+       <UploadForm />
+      </div>
       </div>
     </section>
   );
