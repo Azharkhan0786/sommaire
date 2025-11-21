4ae41dd5ef9c083ec44a2a4ccb7245e4351f03ce
Azhar Khan <azharkhan844716@gmail.com>
2025-11-22 00:47:09 +0530

    commit

--- components/upload/upload-form.tsx
+++ components/upload/upload-form.tsx
@@ -1,39 +1,44 @@
-'use client';
-import {Button} from "@/components/ui/button"
+"use client";
+import { Button } from "@/components/ui/button";
 import UploadFormInput from "./upload-form-input";
-import {z} from 'zod';
+import { z } from "zod";
 
-const schema=z.object({
-    file:z
-    .instanceof(File,{message: "Invalid File"})
-    .refine((file)=>file.size <= 24*1024 *1024,{
-        message:"File size must be less than 24 mb",
+const schema = z.object({
+  file: z
+    .instanceof(File, { message: "Invalid File" })
+    .refine((file) => file.size <= 24 * 1024 * 1024, {
+      message: "File size must be less than 24 mb",
     })
-    .refine((file)=>file.type.startsWith('application/pdf'),
-    {message: "File must be a PDF"}),
+    .refine((file) => file.type.startsWith("application/pdf"), {
+      message: "File must be a PDF",
+    }),
 });
 
-export default function UploadForm(){
+export default function UploadForm() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form submitted");
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get("file") as File;
 
-    const handleSubmit=(e:React.FormEvent<HTMLFormElement>)=>{
-        e.preventDefault();
-        console.log("Form submitted");
-        const formData=new FormData(e.currentTarget);
-        const file=formData.get("file") as File;
+    //validating a file
+    const validatedFields = schema.safeParse({ file });
 
-        //validating a file
-        //schema with zod
-        //upload the file to uploadThing
-        //parse the file using langchain
-        //summarise the pdf using AI
-        //save the summary to the database
-        // redirect ti {id} of the summary page
+    if (!validatedFields.success) {
+      const errors = validatedFields.error.format();
+      console.log(errors.file?._errors[0] ?? "Invalid file");
+      return;
     }
-    return (
-        <div className=" w-full max-w-2xl gap-8 flex flex-col">
+    //schema with zod
+    //upload the file to uploadThing
+    //parse the file using langchain
+    //summarise the pdf using AI
+    //save the summary to the database
+    // redirect ti {id} of the summary page
+  };
+  return (
+    <div className=" w-full max-w-2xl gap-8 flex flex-col">
       <UploadFormInput onSubmit={handleSubmit} />
-        </div>
-    
-    );
-
+    </div>
+  );
 }
