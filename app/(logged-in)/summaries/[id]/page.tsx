6daff5b70b8c0bdeee3ba456cb16e0ae2a12af9e
Azhar Khan <azharkhan844716@gmail.com>
2025-12-10 00:33:15 +0530

    commit

--- app/(logged-in)/summaries/[id]/page.tsx
+++ app/(logged-in)/summaries/[id]/page.tsx
@@ -1,16 +1,45 @@
+import SourceInfo from "@/components/summaries/source-info";
+import SummaryHeader from "@/components/summaries/summary-header";
 import { getSummaryById } from "@/lib/summaries";
 import { notFound } from "next/navigation";
+import { FileText } from "lucide-react";
 
 export default async function SummaryPage(props: {
-    params: Promise<{ id: string }>;
+  params: Promise<{ id: string }>;
 }) {
-    const params=await props.params;
-    const id=params.id;
+  const params = await props.params;
+  const id = params.id;
 
-    const summary=await getSummaryById(id);
+  const summary = await getSummaryById(id);
 
-    if (!summary) {
-        notFound();
-    }
-    return <div className="relative isolate min-h-screen bg-linear-to-r from-rose-50/40 to-white"></div>;
+  if (!summary) {
+    notFound();
+  }
+
+  const { title, summary_text, file_name } = summary;
+
+  return (
+    <div className="relative isolate min-h-screen bg-linear-to-r from-rose-50/40 to-white">
+      <div className="container mx-auto flex flex-col gap-4">
+        <div className="px-4 sm:px-6 lg:px-8 py-8 sm:py-12 lg:py-24">
+          <div className="flex flex-col">
+            <SummaryHeader title={title} />
+          </div>
+          {file_name && <SourceInfo fileName={file_name} />}
+          <div className="relative mt-4 sm:mt-8 lg:mt-16 ">
+            <div className="relative p-4 sm:p-6 lg:p-8 bg-white/80  backdrop-blur-md  rounded-2xl sm:rounded-3xl shadow-xl border border-rose-100/30  transition-all duration-300 hover:shadow-2xl hover:bg-white/90 max-w-4xl mx-auto ">
+              <div className=" absolute inset-0 bg-linear-to-br from-rose-50/50 via-orange-50/30 to-transparent opac50 rounded-2xl sm:rounded-l-3xl" />
+              <div className="absolute top-2 sm:top-4 right-2 sm:right-4 flex items-center gap-1.5 sm:gap-2 text-xs sm:text-sm text-muted-foreground bg-white/90 px-2 sm:px-3 py-1 sm:py-1.5 rounded-full shadow-xs">
+                <FileText className="h-3 w-3 sm:h-4 sm:w-4 text-rose-400" />
+                {word_count?.toLocaleString()}words
+              </div>
+              <div className="relative mt-8 sm:mt-6 flex justify-center">
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
