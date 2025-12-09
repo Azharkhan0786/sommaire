0a42f665297eb1790b0d27901231ae42235b62b0
Azhar Khan <azharkhan844716@gmail.com>
2025-12-10 01:40:13 +0530

    commit

--- components/summaries/summary-header.tsx
+++ components/summaries/summary-header.tsx
@@ -1,3 +1,14 @@
+import { Button } from "../ui/button"
+
 export default function SummaryHeader({title}:{title:string}){
-    return <h1>{title}</h1>
+    return( 
+    <div className="flex gap-4 mb-4 justify-between">
+        <div></div>
+        <div >
+            <Button variant={'link'} size={'sm'} className="">
+
+            </Button>
+        </div>
+    </div>
+    )
 }
