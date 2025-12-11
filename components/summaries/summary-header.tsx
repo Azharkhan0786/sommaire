8ce73028e49e4e0c4d800bf2622b97ed09a964b2
Azhar Khan <azharkhan844716@gmail.com>
2025-12-11 20:28:47 +0530

    commit

--- components/summaries/summary-header.tsx
+++ components/summaries/summary-header.tsx
@@ -10,6 +10,7 @@ export default function SummaryHeader({title}:{title:string}){
             <Button variant={'link'}
              size={'sm'}
               className="group flex items-center gap-1 sm:gap-2 hover:bg-white/80 backdrop-blur-xs rounded-full transition-all duration-200 shadow-xs hover:shadow-md border border-rose-100/30 bg-rose-100 px-2 sm:px-3">
+                <ChevronLeft>
                Back to dashboard
             </Button>
              </Link>
