633e4ca40ceb908aa9a585ef3ae3aa6ae2905550
Azhar Khan <azharkhan844716@gmail.com>
2025-12-15 14:59:38 +0530

    commit

--- components/summaries/source-info.tsx
+++ components/summaries/source-info.tsx
@@ -1,4 +1,5 @@
-import { FileText } from "lucide-react";
+import { ExternalLink, FileText, Ghost } from "lucide-react";
+import { Button } from "../ui/button";
 
 export default function SourceInfo({
   fileName,
@@ -15,9 +16,22 @@ export default function SourceInfo({
 }) {
   return (
     <div className="flex flex-col lg:flex-row items-center justify-between gap-4 text-sm text-muted-foreground">
-        <div className="flex items-center justify-center gap-2">
-            <FileText className="h-4 w-4 text-rose-400"/>
-        </div>
+      <div className="flex items-center justify-center gap-2">
+        <FileText className="h-4 w-4 text-rose-400" />
+        <span>Source:{fileName}</span>
+      </div>
+      <div className="flex gap-2">
+        <Button
+          variant={"ghost"}
+          size={"sm"}
+          className="h-8 px-3 text-rose-600   hover:text-rose-700 hover:bg-rose-50" asChild
+        >
+          <a href={originalFileUrl} target="_blank" rel="noopener norefferer">
+            <ExternalLink className="h-4 w-4 mr-1" />
+            View Original
+          </a>
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
